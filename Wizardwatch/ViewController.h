3d1fe90fc035354355ddb66b3cc69e378504135f
Heather Summy <hsummy@hotmail.com>
2016-11-30 10:16:42 -0500

    Initial Commit

---
+++ Wizardwatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wizardwatch
+//
+//  Created by HSummy on 11/30/16.
+//  Copyright © 2016 HSummy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Wizardwatch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Wizardwatch
+//
+//  Created by HSummy on 11/30/16.
+//  Copyright © 2016 HSummy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
